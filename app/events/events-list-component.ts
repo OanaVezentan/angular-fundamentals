a78543cde6fe320d2d001d5bcd348312cd6cd3b8
VEZENTAN Oana <oana.vezentan@soprasteria.com>
2017-11-12 00:58:40 +0100

    Added an error page, create-event page, event-details page, event route activator, events-list-resolver and route pages using routing and navigation links, guarding against route activation and route de-activation, styled active links, pre-loaded data for components

--- app/events/events-list-component.ts
+++ app/events/events-list-component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from "@angular/router";
+
 @Component ({
-    selector: 'events-list',
     templateUrl: 'app/events/events-list.component.html'
 })
 
 export class EventsListComponent implements OnInit {
-    events: any[];
-    constructor(private eventService: EventService, private toastr: ToastrService) {
+    events: any;
+    constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {
         
     }
 
     ngOnInit() {
-        this.events = this.eventService.getEvents();
+       this.events = this.route.snapshot.data['events']; 
     }
 
     handleThumbnailClick(eventName) {
